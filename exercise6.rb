75c248429bc5634ec6fa07a4d6ae8e8c86876cd7
DocPrior <Sara Prior>
2017-05-15 15:11:30 -0400

    infinite loop with hile loop, no gets

--- exercise6.rb
+++ exercise6.rb
@@ -4,12 +4,11 @@ distance = 0
 puts "walk, run, or go home?"
   answer = gets.to_s
 
-unless answer == "go home"
+while true
     if answer == "walk"
       puts "Distance from home #{distance + 1}km."
     else
-      puts "Distance from home #{distance +5}km."
+      puts "Distance from home #{distance + 5}km."
     end
     puts "walk, run, or go home?"
-      answer = gets.to_s
 end
