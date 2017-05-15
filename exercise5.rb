374eb7f75dc262f158f35f8c3680188089c24566
DocPrior <Sara Prior>
2017-05-15 14:30:05 -0400

    finish exercise 5

--- exercise5.rb
+++ exercise5.rb
@@ -2,9 +2,12 @@ puts "How many pizzas do you want to order?"
 
 pizzas = gets.to_i
 
-puts "How many toppings do you want?"
 
-toppings = gets.to_i
+while pizzas > 0
+  puts "How many topings on pizza number #{pizzas}?"
+  toppings = gets.to_i
+  puts "You ordered a pizza with #{toppings} topping(s)."
+  pizzas -= 1
+end
 
-puts pizzas
-puts toppings
+puts "Thank you for your ordder!"
