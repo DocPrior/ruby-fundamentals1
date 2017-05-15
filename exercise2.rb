e38a649fc248a8d7686901b5f47932973dd5a3f5
DocPrior <Sara Prior>
2017-05-15 14:04:53 -0400

    put files in weird directory, fix now so new commit

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+# calculate tip
+puts 55 * 0.15
+
+puts "meow" + 5.to_s
+
+puts "45628 times 7839 is #{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
---
+++ exercise3.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+
+name = gets
+
+puts "Hi #{name}!"
+
+puts "How old are you?"
+
+birth = gets
+
+puts "You were born in #{2017 - (birth.to_i)}."
