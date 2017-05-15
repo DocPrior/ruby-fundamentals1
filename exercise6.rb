8b150d8e969cc7e5c76329d881166b4a84c45a16
DocPrior <Sara Prior>
2017-05-15 15:25:37 -0400

    remove while loop to trouble shoot if statement

--- exercise6.rb
+++ exercise6.rb
@@ -1,14 +1,10 @@
-
 distance = 0
 
 puts "walk, run, or go home?"
   answer = gets.to_s
 
-while true
-    if answer == "walk"
-      puts "Distance from home #{distance + 1}km."
-    else
-      puts "Distance from home #{distance + 5}km."
-    end
-    puts "walk, run, or go home?"
-end
+if answer == "walk"
+    puts "Distance from home #{distance + 1}km."
+  else
+    puts "Distance from home #{distance + 5}km."
+  end
